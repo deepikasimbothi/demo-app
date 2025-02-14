a6e9726233ff62974cb3f0dc6989847131a29dff
deepikasimbothi <deepikasimbothi@gmail.com>
2025-02-14 16:38:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('🎉 Hello from your AWS EC2 instance! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
