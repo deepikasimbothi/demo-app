043129c3aedf65d13a08d9027935898bd57c9465
deepikasimbothi <deepikasimbothi@gmail.com>
2025-03-16 19:43:19 +0530

    small chnages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('🎉 Hello from your AWS EC2 instance! 🚀');
